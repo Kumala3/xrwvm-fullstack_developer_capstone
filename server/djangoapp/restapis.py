617ae6bb8f6940ef323362dfd1c99c65e110ef5a
Kostek <ksytnik73kwer@gmail.com>
2024-07-08 19:13:54 +0200

    Implement get_request function

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,18 +1,31 @@
-# Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url", default="http://localhost:5050/"
+)
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
+
+def get_request(endpoint: str, **kwargs):
+    params = ""
+    if kwargs:
+        for key, value in kwargs.items():
+            params = f"{params}{key}={value}&"
+
+    request_url = f"{backend_url}{endpoint}?{params}"
+
+    print(f"GET request from {request_url} ")
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception:
+        print("Network exception occurred")  # If any error occurs
+
 
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
