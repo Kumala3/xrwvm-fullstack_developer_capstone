db4a4e73451581363f6392bbb57864e1c8b837bc
Kostek <ksytnik73kwer@gmail.com>
2024-07-08 19:55:23 +0200

    Refactor djangoapp/urls.py to include new endpoints and views

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,15 +1,34 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import login_user, logout_request, registration, get_dealerships, get_cars
+from .views import (
+    login_user,
+    logout_request,
+    registration,
+    get_cars,
+    get_dealerships,
+    get_dealer_details,
+    get_dealer_reviews,
+    add_review,
+)
 
 app_name = "djangoapp"
 urlpatterns = [
     path("login", view=login_user, name="login"),
     path("logout", view=logout_request, name="logout"),
     path("register", view=registration, name="register"),
-    path("dealerships/", view=get_dealerships, name="get_dealerships"),
+    path("get_dealers/", view=get_dealerships, name="dealers"),
+    path("get_dealers/<str:state>", view=get_dealerships, name="dealers_by_state"),
+    path(
+        "get_dealers/<int:dealer_id>",
+        view=get_dealer_details,
+        name="dealer_details",
+    ),
+    path(
+        "get_reviews/<int:dealer_id>",
+        view=get_dealer_reviews,
+        name="dealer_reviews_by_dealer_id",
+    ),
     path("get_cars/", view=get_cars, name="get_cars"),
-    # path for dealer reviews view
-    # path for add a review view
+    path("", view=add_review, name="add_review"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
